6779fdc93d2da799963d5a04576aa700d5af2d2a
Elexy <alex@seretalabs.com>
2018-01-05 12:32:24 -0500

    fix port for demo app

--- contrib/demo/app.js
+++ contrib/demo/app.js
@@ -3,4 +3,4 @@ const app = express()
 
 app.use('/', express.static('static'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(8080, () => console.log('Example app listening on port 8080!'))
