7dfc6de07171378671ae4ccc10d30b0869083c12
Elexy <alex@seretalabs.com>
2018-07-18 12:56:32 +0200

    added archive and upload

--- magefile.dockerapi.go
+++ magefile.dockerapi.go
@@ -13,7 +13,7 @@ import (
 	"github.com/docker/docker/client"
 	"github.com/docker/docker/pkg/jsonmessage"
 	"github.com/docker/docker/pkg/term"
-	"github.com/jhoonb/archivex"
+	"github.com/mholt/archiver"
 )
 
 // mg contains helpful utility functions, like Deps
@@ -27,22 +27,22 @@ const defaultDockerAPIVersion = "v1.37"
 // Build the container using the native docker api
 func Build() error {
 	s := ParseEnvVars()
-	InstallSources(s)
-	tar := new(archivex.TarFile)
-	tar.Create("/tmp/nodejs-distro.tar")
-	tar.AddAll("contrib", true)
-	tar.AddAll("src", true)
-	tar.AddAll("test", true)
-	tar.AddAll("s2i", true)
-	tar.AddAll("help", true)
-	tar.AddAll("image", true)
-	tar.AddAll("licenses", true)
-	tar.Close()
+	InstallSources()
+	err := archiver.Tar.Make("/tmp/nodejs-distro.tar", []string{
+		"contrib",
+		"src",
+		"test",
+		"s2i",
+		"help",
+		"image",
+		"licenses",
+	})
 	dockerBuildContext, err := os.Open("/tmp/nodejs-distro.tar")
 	defer dockerBuildContext.Close()
 	cli, _ := client.NewClientWithOpts(client.WithVersion(defaultDockerAPIVersion))
+	pb := preBuiltEnv(s)
 	args := map[string]*string{
-		"PREBUILT":     &s.Prebuilt,
+		"PREBUILT":     &pb,
 		"NODE_VERSION": &s.Nodeversion,
 	}
 	options := types.ImageBuildOptions{
@@ -55,9 +55,7 @@ func Build() error {
 		BuildArgs:      args,
 	}
 	buildResponse, err := cli.ImageBuild(context.Background(), dockerBuildContext, options)
-	if err != nil {
-		fmt.Printf("%s", err.Error())
-	}
+	check(err)
 	defer buildResponse.Body.Close()
 	fmt.Printf("********* %s **********\n", buildResponse.OSType)
 
@@ -101,10 +99,43 @@ func Publish() error {
 		RegistryAuth: authStr,
 		All:          true,
 	}
-	pushResponse, err := cli.ImagePush(context.Background(), s.Imagename+":"+s.Imagetag, options)
-	if err != nil {
-		fmt.Printf("%s", err.Error())
+	pushResponse, err := cli.ImagePush(context.Background(), imageName(s), options)
+	check(err)
+	defer pushResponse.Close()
+
+	termFd, isTerm := term.GetFdInfo(os.Stderr)
+	return jsonmessage.DisplayJSONMessagesStream(pushResponse, os.Stderr, termFd, isTerm, nil)
+}
+
+// publish to Red Hat Catalog
+func PublishRedHat() error {
+	s := ParseEnvVars()
+	cli, _ := client.NewClientWithOpts(client.WithVersion(defaultDockerAPIVersion))
+
+	authConfig := types.AuthConfig{
+		Username:      "unused",
+		Password:      s.Rhsecret,
+		ServerAddress: s.Rhendpoint,
 	}
+	fmt.Println(authConfig)
+	encodedJSON, err := json.Marshal(authConfig)
+	if err != nil {
+		panic(err)
+	}
+	authStr := base64.URLEncoding.EncodeToString(encodedJSON)
+	authBody, err := cli.RegistryLogin(context.Background(), authConfig)
+	check(err)
+	fmt.Println(authBody)
+
+	options := types.ImagePushOptions{
+		RegistryAuth: authStr,
+		All:          true,
+	}
+	RedHatImageName := s.Rhendpoint + "/" + s.Rhproject + "/nearform-s2i-node" + ":" + s.Imagetag
+	err = cli.ImageTag(context.Background(), imageName(s), RedHatImageName)
+	check(err)
+	pushResponse, err := cli.ImagePush(context.Background(), RedHatImageName, options)
+	check(err)
 	defer pushResponse.Close()
 
 	termFd, isTerm := term.GetFdInfo(os.Stderr)
--- magefile.go
+++ magefile.go
@@ -4,8 +4,11 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"regexp"
+	"strings"
 
 	"github.com/kelseyhightower/envconfig"
 	"github.com/magefile/mage/sh"
@@ -13,22 +16,29 @@ import (
 )
 
 type Specification struct {
-	Os          string `required:"true"`
-	Nodeversion string `required:"true"`
-	V8version   string `required:"true"`
-	Dockerfile  string `required:"true"`
-	Imagetag    string `required:"true"`
-	Latest      bool   `default:false`
-	Lts         string
-	Majortag    string `required:"true"`
-	Minortag    string `required:"true"`
-	Imagename   string `required:"true"`
-	Npmversion  string `required:"true"`
-	Fromdata    string `required:"true"`
-	Prebuilt    string `default:false`
-	Dockeruser  string `required:"true"`
-	Dockerpass  string `required:"true"`
+	Os                 string `required:"true"`
+	Nodeversion        string `required:"true"`
+	V8version          string `required:"true"`
+	Dockerfile         string `required:"true"`
+	Imagetag           string `required:"true"`
+	Latest             bool   `default:false`
+	Lts                string
+	Majortag           string `required:"true"`
+	Minortag           string `required:"true"`
+	Imagename          string `required:"true"`
+	Npmversion         string `required:"true"`
+	Fromdata           string `required:"true"`
+	Prebuilt           bool   `default:false`
+	Dockeruser         string `required:"true"`
+	Dockerpass         string `required:"true"`
+	Rhsecret           string `required:"true"`
+	Rhendpoint         string `required:"true"`
+	Rhproject          string `required:"true"`
+	AwsAccessKey       string `required:"true" split_words:"true"`
+	AwsSecretAccessKey string `required:"true" split_words:"true"`
 }
+
+const bucketName string = "sourcecode-nearform-export-compliance"
 
 // Default target to run when none is specified
 // If not set, running mage will list available targets
@@ -55,21 +65,56 @@ func ParseEnvVars() Specification {
 		s.Imagename,
 		s.Npmversion,
 		s.Fromdata,
-		s.Prebuilt)
+		s.Prebuilt,
+	)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 	return s
 }
 
-// Get Node.js sources, build
-func InstallSources(s Specification) error {
-	dir, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err)
+// get base image name
+func imageName(s Specification) string {
+	return s.Imagename + ":" + s.Imagetag
+}
+
+func isLatest(s Specification) string {
+	if s.Latest {
+		return "T"
 	}
+	return ""
+}
+
+func preBuiltEnv(s Specification) string {
+	if s.Prebuilt {
+		return "T"
+	}
+	return " "
+}
+
+func archiveName(s Specification) string {
+	dashedImageName := strings.Replace(s.Imagename, "/", "-", -1)
+	return "sources-" + dashedImageName + ".tgz"
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+// show configuratio
+func ShowConfig() {
+	fmt.Println(ParseEnvVars())
+}
+
+// Get Node.js sources, build
+func InstallSources() error {
+	s := ParseEnvVars()
+	dir, err := os.Getwd()
+	check(err)
 	path := dir + "/src/node-v" + s.Nodeversion + "-linux-x64.tar.gz"
-	if s.Prebuilt != "" {
+	if s.Prebuilt {
 		log.Println("checking if " + path + " exists")
 		if _, err := os.Stat(path); os.IsNotExist(err) {
 			runDownloadScript(s)
@@ -85,14 +130,12 @@ func InstallSources(s Specification) error {
 func runDownloadScript(s Specification) error {
 	dir, err := os.Getwd()
 	var envs = map[string]string{
-		"PREBUILT": s.Prebuilt,
+		"PREBUILT": preBuiltEnv(s),
 		"OS":       s.Os,
 	}
 	fmt.Println("Installing Node.js sources...")
 	_, err = sh.Exec(envs, os.Stdout, os.Stdout, "./contrib/etc/get_node_source.sh", s.Nodeversion, dir+"/src/")
-	if err != nil {
-		log.Fatal(err)
-	}
+	check(err)
 	return err
 }
 
@@ -102,24 +145,80 @@ func Squash() {
 	s := ParseEnvVars()
 	tags := getTags(s)
 	var envs = map[string]string{}
-	_, err := sh.Exec(envs, os.Stdout, os.Stdout, "docker-squash", tags[0], "-t", tags[0])
-	if err != nil {
-		log.Fatal(err)
-	}
+	_, err := sh.Exec(envs, os.Stdout, os.Stdout, "docker-squash", tags[0], "-f", fromImage(s), "-t", tags[0])
+	check(err)
 }
 
 // Run a test on the image
 func Test() {
-	fmt.Println("Squashing the image...")
+	fmt.Println("Testing the image...")
 	sh.Rm("src/.")
 	s := ParseEnvVars()
-	fmt.Println("Cleanup image " + s.Imagename + ":" + s.Imagetag)
+	fmt.Println("Cleanup image " + imageName(s))
 	var envs = map[string]string{
-		"BUILDER":      s.Imagename + ":" + s.Imagetag,
+		"BUILDER":      imageName(s),
 		"NODE_VERSION": s.Nodeversion,
 	}
 	_, err := sh.Exec(envs, os.Stdout, os.Stdout, "test/run.sh")
-	if err != nil {
-		log.Fatal(err)
+	check(err)
+}
+
+// create archive with sources
+func Archive() error {
+	s := ParseEnvVars()
+	os.MkdirAll("dist", os.ModePerm)
+	var envs = map[string]string{
+		"ARCHIVE":      archiveName(s),
+		"NODE_VERSION": s.Nodeversion,
+		"OS":           s.Os,
+		"DOCKERFILE":   s.Dockerfile,
+		"IMAGE_TAG":    s.Imagetag,
+		"LATEST":       isLatest(s),
+		"MAJOR_TAG":    s.Majortag,
+		"MINOR_TAG":    s.Minortag,
+		"IMAGE_NAME":   s.Imagename,
+		"NPM_VERSION":  s.Npmversion,
 	}
+	_, err := sh.Exec(envs,
+		os.Stdout,
+		os.Stdout,
+		"contrib/etc/archive.sh",
+	)
+	check(err)
+	return err
+}
+
+// upload archive to S3
+// Upload input parameters
+func Upload() error {
+	// s3cmd put $(ARCHIVE) "$(S3BUCKET)/$(ARCHIVE)"
+	s := ParseEnvVars()
+	var envs = map[string]string{
+		"AWS_ACCESS_KEY_ID":     s.AwsAccessKey,
+		"AWS_SECRET_ACCESS_KEY": s.AwsSecretAccessKey,
+	}
+	_, err := sh.Exec(envs,
+		os.Stdout,
+		os.Stdout,
+		"s3cmd",
+		"put",
+		archiveName(s),
+		"s3://"+bucketName+"/sources/"+archiveName(s),
+	)
+	check(err)
+	return err
+}
+
+// helper functions
+// get the FROM image string
+func fromImage(s Specification) string {
+	b, err := ioutil.ReadFile(s.Dockerfile) // just pass the file name
+	if err != nil {
+		fmt.Print(err)
+	}
+	str := string(b) // convert content to a 'string'
+	re := regexp.MustCompile(`FROM (.*)`)
+	matches := re.FindStringSubmatch(str)
+	fmt.Println("Base image name: " + matches[1])
+	return matches[1]
 }
